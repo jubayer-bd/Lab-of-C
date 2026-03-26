49db38f300e01c66584964861d71cb83f9d9f40d
Md. Jubayer Hossain <mjh84349@gmail.com>
2026-03-26 11:00:43 +0600

    un comment

--- Lab2.c
+++ Lab2.c
@@ -1,37 +1,37 @@
 // Conditional Operator: -------------
 // 1. Find out if a number is odd or even using a conditional operator.
 
-// #include<stdio.h> 
-// int main () {
-//     int num;
+#include<stdio.h> 
+int main () {
+    int num;
 
-//     printf("Enter a number: ");
-//     scanf("%d", &num);
-//     if(num % 2 == 0) 
-//     printf("%d is an even number.", num);
-//     else
-//     printf("%d is an odd number.", num);
-//     return 0;
-// }
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num % 2 == 0) 
+    printf("%d is an even number.", num);
+    else
+    printf("%d is an odd number.", num);
+    return 0;
+}
 
 // 2. Find out the largest between three numbers using the conditional operator.
 
-// #include<stdio.h>
-// int main() {
-//     int num1, num2, num3;
-// printf("Enter three numbers: ");
-// scanf("%d %d %d", &num1, &num2, &num3);
-// if(num1 > num2 && num1 > num3) 
-// printf("%d is the largest number.", num1);
-// else if (num2 > num1 && num2 > num3) 
-// printf("%d is the largest number.", num2);
-// else if(num3 > num1 && num3> num2) 
-// printf("%d is the largest number.", num3);
+#include<stdio.h>
+int main() {
+    int num1, num2, num3;
+printf("Enter three numbers: ");
+scanf("%d %d %d", &num1, &num2, &num3);
+if(num1 > num2 && num1 > num3) 
+printf("%d is the largest number.", num1);
+else if (num2 > num1 && num2 > num3) 
+printf("%d is the largest number.", num2);
+else if(num3 > num1 && num3> num2) 
+printf("%d is the largest number.", num3);
 
-// else printf("Either all numbers are equal or two of them are equal and greater than the third one.");
+else printf("Either all numbers are equal or two of them are equal and greater than the third one.");
    
-//     return 0;
-// }
+    return 0;
+}
 
 // 3. Find out if the input is an alphabet using a conditional operator.
 
